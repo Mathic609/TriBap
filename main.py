9693c7baacbd61d280ad4ab7f26822281b4435af
Matic <matic.johnpatrick2040@gmail.com>
2022-04-06 14:32:26 +0800

    Bootstrap, HTML, and CSS.

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///review.db")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Review(db.Model):
+    __tablename__ = "reviews"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.String(250), nullable=False)
+    review = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+db.create_all()
+
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    No_of_files = len(os.listdir("static/assets/promo")) - 1
+    reviews = Review.query.all()
+    return render_template("index.html", n=No_of_files, reviews=reviews)
 
 
 if __name__ == "__main__":
