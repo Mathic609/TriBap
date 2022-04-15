cea5d4d0c351ff75df38c9d09cf2be91e466d471
Matic <matic.johnpatrick2040@gmail.com>
2022-04-15 16:20:31 +0800

    Bootstrap, HTML, and CSS.

--- main.py
+++ main.py
@@ -33,5 +33,15 @@ def home():
     return render_template("index.html", n=No_of_files, reviews=reviews)
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
